9747bf5d87d13d3ce6b834635641bdcae6f91430
ProximoBinks <elliot.koh@student.adelaide.edu.au>
2024-01-02 22:35:08 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,10 +13,16 @@ public:
     // Constructor initializing the graph with a given number of vertices
     Graph(int vertices) :numVertices(vertices), adjLists(vertices) {}
     
-    // Function to add an edge between two vertices (undirected)
+    // Function to add an edge between two vertices
     void addEdge(int src, int dest) {
         adjLists[src].push_back(dest);
         adjLists[dest].push_back(src);
+    }
+
+    // Function to delete an edge between two vertices
+    void deleteEdge(int src, int dest) {
+        adjLists[src].remove(dest); // Remove dest from src's adjacency list
+        adjLists[dest].remove(src); // Remove src from dest's adjacency list
     }
 
     // Function to perform BFS starting from vertex s
@@ -53,8 +59,14 @@ int main() {
     g.addEdge(1, 3);
     g.addEdge(1, 4);
 
-    cout << "Breadth First Traversal (starting from vertex 0): \n";
-    g.BFS(0); // Perform BFS starting from vertex 0
+    cout << "Breath First Traversal (starting from vertex 0): \n";
+    g.BFS(0);
+
+    cout << "\nDeleting edge (1, 4)\n";
+    g.deleteEdge(1, 4); // Delete edge between vertex 1 and 4
+
+    cout << "Breath First Traversal after deleting edge (starting from vertex 0): \n";
+    g.BFS(0);
 
     return 0; // End of the program
 }
