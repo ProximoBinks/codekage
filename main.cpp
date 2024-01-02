ef2d2cc6cebe75787cf8ae4a4ff9ba4eb6d81e47
ProximoBinks <elliot.koh@student.adelaide.edu.au>
2024-01-02 19:49:24 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,33 +6,39 @@
 using namespace std;
 
 class Graph {
-    int numVertices;
-    vector<list<int>> adjLists;
+    int numVertices; // Number of vertices in the graph
+    vector<list<int>> adjLists; // Adjacency lists for each vertex
 
 public:
+    // Constructor initializing the graph with a given number of vertices
     Graph(int vertices) :numVertices(vertices), adjLists(vertices) {}
     
+    // Function to add an edge between two vertices (undirected)
     void addEdge(int src, int dest) {
         adjLists[src].push_back(dest);
         adjLists[dest].push_back(src);
     }
 
+    // Function to perform BFS starting from vertex s
     void BFS(int s) {
-        vector<bool> visited(numVertices, false);
-        queue<int> queue;
+        vector<bool> visited(numVertices, false); // Keep track of visited vertices
+        queue<int> queue; // Queue to manage the order of visiting vertices
 
-        visited[s] = true;
-        queue.push(s);
+        visited[s] = true; // Mark the starting vertex as visited
+        queue.push(s); // Enqueue the starting vertex
 
+        // Continue until the queue is empty
         while (!queue.empty()) {
-            s = queue.front();
-            cout << s << " ";
-            queue.pop();
+            s = queue.front(); // Get the front vertex in the queue
+            cout << s << " "; // Print the current vertex
+            queue.pop(); // Dequeue
 
-            for (auto adj :adjLists[s]) {
+            // Go through the adjacency list of the current vertex
+            for (auto adj : adjLists[s]) {
+                // If an adjacent vertex hasn't been visited
                 if (!visited[adj]) {
-                    visited[adj] = true;
-                    queue.push(adj);
+                    visited[adj] = true; // Mark it as visited
+                    queue.push(adj); // Enqueue it
                 }
             }
         }
@@ -40,14 +46,15 @@ public:
 };
 
 int main() { 
-    Graph g(5);
+    Graph g(5); // Create a graph with 5 vertices
+    // Add edges to the graph
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 3);
     g.addEdge(1, 4);
 
-    cout << "Breath First Traversal (starting from vertex 0): \n";
-    g.BFS(0);
+    cout << "Breadth First Traversal (starting from vertex 0): \n";
+    g.BFS(0); // Perform BFS starting from vertex 0
 
-    return 0;
+    return 0; // End of the program
 }
